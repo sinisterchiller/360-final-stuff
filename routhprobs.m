96fa0c1a61b5e3bcf9d84c1449c0029dd1aca4dc
Anurag Biswas Koushik <anurag.koushik69@gmail.com>
2026-04-12 14:06:50 -0600

    routh problem exceptions

--- routhprobs.m
+++ routhprobs.m
@@ -20,12 +20,12 @@ R(2,:) = [1 2 15];
 
 function value = routh(R,x,y)
     if ((R(x-1,1) == 0))
-        value = sym('u')
+        value = sym('u');
         return
     end
     value = simplify((R(x-1,1)*R(x-2,y+1) - R(x-2,1)*R(x-1,y+1)) / R(x-1,1));
     if (value == 0)
-        value = sym('u')
+        value = sym('u');
         return
     end
 end
@@ -38,5 +38,8 @@ R(4,2) = routh(R, 4, 2);
 
 R(5,1) = routh(R, 5, 1);
 R(5,2) = routh(R, 5, 2);
+
+R(6,1) = routh(R, 6, 1);
+R(6,2) = routh(R, 6, 2);
 
 pretty(R)
